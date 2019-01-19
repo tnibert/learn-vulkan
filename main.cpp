54651da23d99fa982534b1ecc3c101e6ec7ae78f
Tim Nibert <docz2a@gmail.com>
2019-01-19 20:26:33 +1100

    created app skeleton, added some rhyme and reason

--- main.cpp
+++ main.cpp
@@ -1,35 +1,67 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+//#include <vulkan/vulkan.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
 #include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const char * TITLE = "Vulkan";
+
+class HelloTriangleApplication
+{
+    public:
+        void run()
+        {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+    private:
+        GLFWwindow * window;
+        void initWindow()
+        {
+            glfwInit();
+            // tell glfw to not use opengl
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            // disable window resizing
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+            window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
+        }
+        void initVulkan()
+        {
+
+        }
+
+        void mainLoop()
+        {
+            while (!glfwWindowShouldClose(window))
+            {
+                glfwPollEvents();
+            }
+        }
+
+        void cleanup()
+        {
+            glfwDestroyWindow(window);
+            glfwTerminate();
+        }
+};
 
 int main()
 {
-    glfwInit();
+    HelloTriangleApplication app;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported" << std::endl;
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
