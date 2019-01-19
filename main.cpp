781493c469acb4a4e7adf2c2d46df33b1c86933e
Tim Nibert <docz2a@gmail.com>
2019-01-19 23:39:18 +1100

    initialized vulkan

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
-const char * TITLE = "Vulkan";
+const char * TITLE = "Hello Triangle";
 
 class HelloTriangleApplication
 {
@@ -24,6 +24,7 @@ class HelloTriangleApplication
 
     private:
         GLFWwindow * window;
+        VkInstance instance;
         void initWindow()
         {
             glfwInit();
@@ -35,7 +36,7 @@ class HelloTriangleApplication
         }
         void initVulkan()
         {
-
+            createInstance();
         }
 
         void mainLoop()
@@ -50,6 +51,33 @@ class HelloTriangleApplication
         {
             glfwDestroyWindow(window);
             glfwTerminate();
+        }
+
+        void createInstance()
+        {
+            // create vulkan app information structure
+            VkApplicationInfo appInfo = {};
+            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            appInfo.pApplicationName = TITLE;
+            appInfo.pEngineName = "No Engine";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_0;
+
+            VkInstanceCreateInfo createInfo = {};
+            createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+            createInfo.pApplicationInfo = &appInfo;
+            uint32_t glfwExtensionCount = 0;
+            const char** glfwExtensions;
+            glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+            createInfo.enabledExtensionCount = glfwExtensionCount;
+            createInfo.ppEnabledExtensionNames = glfwExtensions;
+            createInfo.enabledLayerCount = 0;
+
+            // actually create the instance and check error
+            // vkCreateInstance() returns type VkResult
+            if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create instance!");
+            }
         }
 };
 
