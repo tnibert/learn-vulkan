47ed8d4787f0ff97362680139f9e6e1e4546564f
Tim Nibert <docz2a@gmail.com>
2019-12-23 22:51:37 +1100

    scaffold for getting physical device

--- main.cpp
+++ main.cpp
@@ -61,10 +61,48 @@ class HelloTriangleApplication
             glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
             window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
         }
+
         void initVulkan()
         {
             createInstance();
             setupDebugCallback();
+	    pickPhysicalDevice();
+        }
+
+        void pickPhysicalDevice()
+        {
+            VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	    uint32_t deviceCount = 0;
+            vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+	    if (deviceCount == 0)
+	    {
+                throw std::runtime_error("failed to find GPUs with Vulkan support!");
+            }
+
+	    std::vector<VkPhysicalDevice> devices(deviceCount);
+            vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	    for (const auto& device : devices)
+	    {
+                if (isDeviceSuitable(device))
+		{
+                    physicalDevice = device;
+                    break;
+                }
+            }
+
+            if (physicalDevice == VK_NULL_HANDLE)
+	    {
+                throw std::runtime_error("failed to find a suitable GPU!");
+            }
+
+	    
+        }
+
+        bool isDeviceSuitable(VkPhysicalDevice device)
+        {
+            return true;
         }
 
         void setupDebugCallback()
