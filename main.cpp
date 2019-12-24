959ea1ff17f1af3006ae9d3b2e669b3ae1a0129e
Tim Nibert <docz2a@gmail.com>
2019-12-24 17:44:33 +1100

    check swap chain for adequate capabilities

--- main.cpp
+++ main.cpp
@@ -49,6 +49,13 @@ struct QueueFamilyIndices
     }
 };
 
+struct SwapChainSupportDetails
+{
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
 class HelloTriangleApplication
 {
     public:
@@ -201,7 +208,14 @@ class HelloTriangleApplication
 
             bool extensionsSupported = checkDeviceExtensionSupport(device);
 
-            return indices.isComplete() && extensionsSupported;
+            bool swapChainAdequate = false;
+            if (extensionsSupported)
+            {
+                SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+                swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+            }
+
+            return indices.isComplete() && extensionsSupported && swapChainAdequate;
         }
 
         bool checkDeviceExtensionSupport(VkPhysicalDevice device)
@@ -258,6 +272,33 @@ class HelloTriangleApplication
 
 	        return indices;
 	    }
+
+        SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
+        {
+            SwapChainSupportDetails details;
+
+            vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+            uint32_t formatCount;
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+            if (formatCount != 0)
+            {
+                details.formats.resize(formatCount);
+                vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+            }
+
+            uint32_t presentModeCount;
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+            if (presentModeCount != 0)
+            {
+                details.presentModes.resize(presentModeCount);
+                vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+            }
+
+            return details;
+        }
 
         void setupDebugCallback()
         {
